d174d149b3b4df3001b4545268a850ed63dd3e3d
Kev <kev162020@gmail.com>
2023-04-09 14:11:15 -0600

    es10

---
+++ src/es10/01-trimstart-trimsend.js
@@ -0,0 +1,3 @@
+const hello = '       Hola       ';
+console.log(hello.trimStart());
+console.log(hello.trimEnd());
---
+++ src/es10/03-fromentries.js
@@ -0,0 +1,3 @@
+const entries = new Map([["name", "kevin"], ["age", 22]]);
+console.log(entries);
+console.log(Object.fromEntries(entries));
